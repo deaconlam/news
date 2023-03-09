1e7d5548e1e327312ccd0d8a5b1eb86f667189b4
deaconlam <127363839+deaconlam@users.noreply.github.com>
2023-03-08 18:09:13 -0800

    Add files via upload

---
+++ wpnrw.js
@@ -0,0 +1,45 @@
+function home() {
+    location = "index.html";
+}
+function global() {
+    location = "wpnrw-global.html"
+}
+function sport() {
+    location = "wpnrw-sport.html"
+}
+function travel() {
+    location = "wpnrw-travel.html"
+}
+function features() {
+    location = "wpnrw-features.html"
+}
+function reflections() {
+    location = "wpnrw-reflections.html"
+}
+function article() {
+    location = "wpnrw-article.html"
+}
+function article1() {
+    window.open("https://www.bbc.com/news/world-us-canada-63631316", '_blank').focus();
+}
+function article2() {
+    window.open("https://www.bbc.com/news/world-middle-east-63636783", '_blank').focus();
+}
+function article3() {
+    window.open("https://www.bbc.com/sport/football/63736679", '_blank').focus();
+}
+function article4() {
+    window.open("https://www.bbc.com/sport/football/63759088", '_blank').focus();
+}
+function article5() {
+    window.open("https://www.bbc.com/sport/football/63763638", '_blank').focus();
+}
+function article6() {
+    window.open("https://www.bbc.com/travel/article/20221122-zimbabwes-stunning-80km-safari-train", '_blank').focus();
+}
+function article7() {
+    window.open("https://www.bbc.com/travel/article/20221121-mahua-the-indian-liquor-the-british-banned", '_blank').focus();
+}
+function article8() {
+    window.open("https://www.bbc.com/travel/article/20221120-in-charleston-a-museum-2300-years-in-the-making", '_blank').focus();
+}
